d9805307cc2cc5e3b0f1fca731a66efbfd0e7196
User <User@DESKTOP-QM78EKU>
2021-02-09 20:37:18 +0600

    this is swap variable

---
+++ swap variable/src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+			String x="black color";
+			String y="white color";
+			String temp;
+			
+			temp=x;
+			x=y;
+			y=temp;
+			
+			
+			System.out.println("x:"+x);
+			System.out.println("y:"+y);
+
+	}
+
+}
